bd82f38e8d7360e637996543d7f9b3b445bbf560
Caio <caio.vsg@hotmail.com>
2022-03-11 11:19:20 -0300

    Adicionando informações de Visitas

--- lib/core/api.dart
+++ lib/core/api.dart
@@ -1,11 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:meioambientemobile/core/user_model.dart';
+import 'package:meioambientemobile/core/models/user_model.dart';
 import 'package:provider/provider.dart';
 
 class Api {
   final _dio = Dio();
   static const String baseUrl = 'http://77.243.85.238:8003/api';
+
   Future login(BuildContext context, String email, String password) async {
     final UserModel userModel = Provider.of<UserModel>(context, listen: false);
     try {
@@ -28,6 +29,31 @@ class Api {
       }
     } catch (e) {
       return false;
+    }
+  }
+
+  //Requisita todos os dados de visitas
+  Future getAllVisits(BuildContext context) async {
+    final userModel = Provider.of<UserModel>(context);
+    try {
+      var response = await _dio.get(
+        baseUrl + '/visitas',
+        options: Options(
+          headers: {
+            'Authorization': 'Bearer ${userModel.refreshToken}',
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+          },
+        ),
+      );
+      print(response.data);
+      if (response.statusCode == 200) {
+        return (response.data);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print(e);
     }
   }
 }
