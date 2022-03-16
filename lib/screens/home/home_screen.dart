2d943b2f8cc3712a6e00765df32471b17a9b550e
José Daniel Florêncio Duarte Filho <danielduartefilho@gmail.com>
2022-03-16 17:52:10 -0300

    Profile Screen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -66,7 +66,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       );
                     }),
-                    itemCount: 8,
+                    itemCount: 1,
                   ),
                 ),
               ],
