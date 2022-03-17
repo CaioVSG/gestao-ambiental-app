13cb3bbc1e09f5994c340def9592927dc30a9792
Baladowski <danielduartefilho.df@gmail.com>
2022-03-17 16:30:06 -0300

    finished home screen

--- lib/core/models/visits_model.dart
+++ lib/core/models/visits_model.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 class VisitsModel with ChangeNotifier {
+  //**DADOS DA VISITAS */
   final String visitDate;
-  VisitsModel({required this.visitDate});
+  final String createdAt;
+
+  VisitsModel({
+    required this.visitDate,
+    required this.createdAt,
+  });
 }
