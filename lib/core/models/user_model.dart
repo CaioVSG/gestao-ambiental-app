bc40d39df9cc8253419c1d51dbd12afb835809e1
Caio <caio.vsg@hotmail.com>
2022-04-12 13:23:21 -0300

    Adicionando campos extra do user

--- lib/core/models/user_model.dart
+++ lib/core/models/user_model.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
 
 class UserModel with ChangeNotifier {
+  int id = 0;
   String refreshToken = '';
+  String name = '';
+  String email = '';
+  String profilePhotoUrl = '';
   void setUserRefreshToken(String refreshToken) {
     this.refreshToken = refreshToken;
+    notifyListeners();
+  }
+
+  void setUserName(String name) {
+    this.name = name;
+    notifyListeners();
+  }
+
+  void setUserEmail(String email) {
+    this.email = email;
+    notifyListeners();
+  }
+
+  void setUserId(int id) {
+    this.id = id;
+    notifyListeners();
+  }
+
+  void setUserProfilePhotoUrl(String profilePhotoUrl) {
+    this.profilePhotoUrl = profilePhotoUrl;
     notifyListeners();
   }
 }
